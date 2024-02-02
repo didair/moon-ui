74cdb0795d45dc3c44d61d71f9a427501b419abf
Andreas Ekström <didair@msn.com>
2024-02-02 16:48:06 +0100

    Add signals support in render

--- src/render.ts
+++ src/render.ts
@@ -1,6 +1,6 @@
 import { Signal } from "@preact/signals-core";
 import { applyElementStyles, handleElementLifecycles, applyElementAttributes, ElementProps } from "./element";
-import { getSignalChildren, isSignal } from "./signals";
+import { getSignalChildren, getSignalsInProps, isSignal } from "./signals";
 
 export const render = (elementList: Array<ElementProps>, parent: HTMLElement = null) => {
 	elementList.forEach(async (element) => {
@@ -48,9 +48,12 @@ const renderElement = (element: ElementProps, parent = null) => {
 	if (element.children != null) {
 		renderElementChildren(element, node);
 
-		const signals = getSignalChildren(element.children);
+		const signals = [
+			...getSignalChildren(element.children),
+			...getSignalsInProps(element.props),
+		];
+
 		if (signals.length > 0) {
-			console.log('signals in element', element, signals);
 			signals.forEach((signal: Signal) => {
 				signal.subscribe((value) => {
 					onSignalChange(value, element, node);
@@ -72,23 +75,30 @@ const renderElement = (element: ElementProps, parent = null) => {
 };
 
 const renderElementChildren = (element: ElementProps, node) => {
-	if (Array.isArray(element.children)) {
-		render(element.children, node);
-	}
-	
-	if (!Array.isArray(element.children) && typeof element.children === 'object') {
-		// Convert to array
-		render([element.children], node);
+	let children = element.children;
+	if (typeof element.children == 'function') {
+		children = element.children(element.props);
 	}
 
-	if (typeof element.children === 'string') {
-		node.innerText = element.children;
+	if (Array.isArray(children)) {
+		render(children, node);
+	}
+	
+	if (!Array.isArray(children) && typeof children === 'object') {
+		// Convert to array
+		render([children], node);
+	}
+
+	if (typeof children === 'string') {
+		node.innerText = children;
 	}
 };
 
 const onSignalChange = (value, element, node) => {
 	node.innerHTML = '';
 	renderElementChildren(element, node);
+	applyElementAttributes(element, node);
+	applyElementStyles(element, node);
 };
 
 const renderText = (text: string, parent = null) => {
