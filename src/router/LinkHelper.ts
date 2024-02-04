a3d4733733301cfdab0e1d2c1160c89e9ee1cea2
Andreas Ekström <didair@msn.com>
2024-02-04 13:25:11 +0100

    Updates on LinkHelper isExternal

--- src/router/LinkHelper.ts
+++ src/router/LinkHelper.ts
@@ -1,6 +1,11 @@
 import { Router } from "./Router";
 
-const isExternalURL = (url) => new URL(url).origin !== location.origin;
+const isExternal = (url) => {
+	var match = url.match(/^([^:\/?#]+:)?(?:\/\/([^\/?#]*))?([^?#]+)?(\?[^#]*)?(#.*)?/);
+	if (typeof match[1] === "string" && match[1].length > 0 && match[1].toLowerCase() !== location.protocol) return true;
+	if (typeof match[2] === "string" && match[2].length > 0 && match[2].replace(new RegExp(":("+{"http:":80,"https:":443}[location.protocol]+")?$"), "") !== location.host) return true;
+	return false;
+}
 
 export const attachAnchorEvents = () => {
 	if ((window as any).routerContext == null) {
@@ -12,7 +17,7 @@ export const attachAnchorEvents = () => {
 	return {
 		onClick: (event: Event) => {
 			const href = (event.target as HTMLAnchorElement).getAttribute('href');
-			if (!isExternalURL(href)) {
+			if (href != null && !isExternal(href)) {
 				event.preventDefault();
 				router.navigate(href);
 			}
